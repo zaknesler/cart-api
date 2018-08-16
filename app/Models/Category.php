5e20cc990215d91580df9a2e73e2f0aeecb110d2
Zak Nesler <zaknesler@gmail.com>
2018-08-16 14:01:50 -0400

    Add sortable categories

---
+++ app/Models/Category.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    /**
+     * Get the categories in proper order.
+     *
+     * @param  Illuminate\Database\Eloquent\Builder $builder
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOrdered(Builder $builder, $direction = 'asc')
+    {
+        return $builder->orderBy('order', $direction);
+    }
+
+    /**
+     * Get the categories that are not children of other categories.
+     *
+     * @param  Illuminate\Database\Eloquent\Builder $builder
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeParents(Builder $builder)
+    {
+        return $builder->whereNull('parent_id');
+    }
+
+    /**
+     * Get the child categories.
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+}
---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+use App\Models\Category;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_category_can_have_many_children()
+    {
+        $categoryA = factory(Category::class)->create();
+
+        $categoryA->children()->save(
+            $categoryB = factory(Category::class)->create()
+        );
+
+        $this->assertEquals($categoryB->id, $categoryA->children()->first()->id);
+        $this->assertEquals($categoryA->id, $categoryB->parent_id);
+    }
+
+    /** @test */
+    function categories_can_be_sorted_by_only_parents()
+    {
+        $categoryA = factory(Category::class)->create();
+
+        $categoryA->children()->save(
+            $categoryB = factory(Category::class)->create()
+        );
+
+        $this->assertEquals(1, Category::parents()->count());
+    }
+
+    /** @test */
+    function categories_can_be_ordered()
+    {
+        $categoryA = factory(Category::class)->create([ 'order' => 2 ]);
+        $categoryB = factory(Category::class)->create([ 'order' => 1 ]);
+
+        $this->assertEquals($categoryB->id, Category::ordered()->first()->id);
+    }
+}
