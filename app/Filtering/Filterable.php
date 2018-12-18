e942317a95b6dcb3bf01da3103ce61b3795cc0ed
Zak Nesler <zaknesler@gmail.com>
2018-12-18 15:43:48 -0500

    Add docblock

--- app/Filtering/Filterable.php
+++ app/Filtering/Filterable.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait Filterable
 {
+    /**
+     * A model can apply filters to its queries.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  array  $filters
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeWithFilters(Builder $builder, $filters = [])
     {
         return (new RequestFilter(request()))->apply($builder, $filters);
