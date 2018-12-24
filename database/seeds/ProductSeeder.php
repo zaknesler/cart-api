bddc1fde3cf021a1f72daa04427d8b6da7dbef80
Zak Nesler <zaknesler@gmail.com>
2018-12-24 14:23:30 -0500

    Improve seeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\ProductVariation;
 use App\Models\ProductVariationType;
@@ -14,12 +15,25 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        $product = factory(Product::class)->create([
-            'name' => 'Coffee',
-            'slug' => 'coffee',
-            'description' => 'Some delicious coffee for you.',
-            'price' => 1000,
+        $this->coffee();
+        $this->shirt();
+    }
+
+    private function coffee()
+    {
+        $category = factory(Category::class)->create([
+            'name' => 'Food',
+            'slug' => 'food',
         ]);
+
+        $category->products()->save(
+            $product = factory(Product::class)->create([
+                'name' => 'Coffee',
+                'slug' => 'coffee',
+                'description' => 'Some delicious coffee for you.',
+                'price' => 1000,
+            ])
+        );
 
         $typeWhole = factory(ProductVariationType::class)->create([
             'name' => 'Whole bean',
@@ -72,6 +86,78 @@ class ProductSeeder extends Seeder
             'product_variation_type_id' => $typeGround->id,
             'name' => '1kg',
             'price' => 3000,
+        ]);
+
+        $category->products()->saveMany(Product::get());
+    }
+
+    private function shirt()
+    {
+        $category = factory(Category::class)->create([
+            'name' => 'Clothing',
+            'slug' => 'clothing',
+        ]);
+
+        $category->products()->save(
+            $product = factory(Product::class)->create([
+                'name' => 'T-shirt',
+                'slug' => 'tshirt',
+                'description' => 'Very comfortable t-shirts.',
+                'price' => 4500,
+            ])
+        );
+
+        $typeBlue = factory(ProductVariationType::class)->create([
+            'name' => 'Blue',
+        ]);
+
+        factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'product_variation_type_id' => $typeBlue->id,
+            'name' => 'small',
+            'price' => null,
+        ]);
+
+        factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'product_variation_type_id' => $typeBlue->id,
+            'name' => 'medium',
+            'price' => 5000,
+        ]);
+
+
+        factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'product_variation_type_id' => $typeBlue->id,
+            'name' => 'large',
+            'price' => 5500,
+        ]);
+
+        // Ground
+
+        $typeRed = factory(ProductVariationType::class)->create([
+            'name' => 'Red',
+        ]);
+
+        factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'product_variation_type_id' => $typeRed->id,
+            'name' => 'small',
+            'price' => null,
+        ]);
+
+        factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'product_variation_type_id' => $typeRed->id,
+            'name' => 'medium',
+            'price' => 5000,
+        ]);
+
+        factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'product_variation_type_id' => $typeRed->id,
+            'name' => 'large',
+            'price' => 5500,
         ]);
     }
 }
