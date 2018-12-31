3ed7c4b3d869df84d1bb46a0f0cb2a6eccdf082b
Zak Nesler <zaknesler@gmail.com>
2018-12-31 15:15:12 -0500

    Add relations to order class

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Address;
+use App\Models\ShippingMethod;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    /**
+     * An order belongs to a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * An order belongs to an address.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    /**
+     * An order belongs to a shipping method.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function shippingMethod()
+    {
+        return $this->belongsTo(ShippingMethod::class);
+    }
 }
