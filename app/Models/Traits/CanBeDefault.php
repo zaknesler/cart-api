c54781d6651007e75544e9691bcd238de4390c5c
Zak Nesler <zaknesler@gmail.com>
2019-01-01 13:49:26 -0500

    Allow boot override

--- app/Models/Traits/CanBeDefault.php
+++ app/Models/Traits/CanBeDefault.php
@@ -9,10 +9,8 @@ trait CanBeDefault
      *
      * @return void
      */
-    protected static function boot()
+    protected static function bootCanBeDefault()
     {
-        parent::boot();
-
         static::creating(function ($model) {
             if ($model->default) {
                 $model->newQuery()->where('user_id', $model->user->id)->update([
